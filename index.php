10fc28855555180316b2f218ac8081d161054f77
PantojaGerson <gersonpantoja22.gp@gmail.com>
2022-05-05 13:09:05 +0800

    website structure has been added

--- index.php
+++ index.php
@@ -7,6 +7,30 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	<div class="wrapper">
+		<div class="header">
+			
+		</div>
+		<div class="nav">
+			
+		</div>
+		<div class="banner">
+			
+		</div>
+		<div class="main">
+			<div class="left-side-bar">
+				
+			</div>
+			<div class="content">
+				
+			</div>
+			<div class="right-side-bar">
+				
+			</div>
+		</div>
+		<div class="footer">
+		</div>
+	</div>
 
 </body>
 </html>
