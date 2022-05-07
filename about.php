772a540a6d7516c1ffe12be66036b97eb1c0f516
PantojaGerson <gersonpantoja22.gp@gmail.com>
2022-05-07 10:48:05 +0800

    about page has been added

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hapag - Filipino Cuisine and Recipes</title>
+	<link rel="stylesheet" href="css/style3.css">
+</head>
+<body>
+	<div class="wrapper-22">
+		<div class="header">
+			<center><a href="index.php"><img src="images/logo.png" class="logo-img" title="logo"></a></center>
+		</div>
+		<div class="nav">
+			<ul><center>
+				<li><a href="index.php" ><b>HOME</b></a></li>
+				<li><a href="recipes.php">RECIPES</a></li>
+				<li><a href="about.php"class="active">ABOUT</a></li>
+			<center></ul>
+		</div>
+		<div class="banner">
+			<img src="images/bannerabout.jpg" title="Pork Adobo">
+		</div>
+		<div class="main">
+			<div class="left-side-bar-23">
+				<h2><center>Content</center></h2>
+					<ul>
+						<li><a href="#home" class="house">About</a></li>
+					</ul>
+			</div>
+			<div class="content">
+				<h1 id="home">About</h1>
+				<div class="description">
+					<?php
+
+						echo "Mabuhay! Welcome To Hapag, My name is Gerson O. Pantoja and I live in Masiga, Gasan, Marinduque but I am ";
+						echo "currently residing with my auntie in Cawit, Boac, Marinduque due to internet connectivity which help me";
+						echo "to do online class. I am a 2nd year college student studying at Marinduque State College pursuing";
+						echo "Bachelor of Science in Information Technology.", "<br/>";
+						echo "<br/>";
+						echo "Since I love food and our project is about website, it brings me in creating this food recipe website but";
+						echo "it is Filipino food recipe only,Why? I want Filipino cuisine to be known around the world not just in our";
+						echo "country. I want the world to know that Filipino food is not just a mixed of influence cuisine of other country that colonize the Philippines. I want them to know that our food has a unique flavor that we can call it ours. I hope people can appreciate Filipino Food through this website that I created which will help them realize that Filipino cuisine is also at par to any cuisine.";
+					?>
+				</div>
+
+				<img src="images/boodle.jpg" title="boodle fight">
+				<h2> Feel free to Contact Us in our social media platforms! </h2>
+
+				
+			</div>
+			<div class="right-side-bar">
+				<ul>
+					<h5>Latest recipes</h5>
+					<li><img src="images/tapsilog.jpg" title="Tapsilog"></li>
+					<h4> Tapsilog </h4>
+					<li><img src="images/halohalo.jpg" class="halohalo" title="Halo-Halo"></li>
+					<h4> Halo-Halo </h4>
+					<li><img src="images/sinigang.jpg" title="Sinigang"></li>
+					<h4> Sinigang </h4>
+					<li><img src="images/bibingka.jpg" title="Bibingka"></li>
+					<h4> Bibingka </h4>
+					<li><img src="images/chopsuey.jpg" title="Chop Suey"></li>
+					<h4> Chop Suey </h4>
+				</ul>
+			</div>
+		</div>
+		<div class="footer">
+			<p><b>&copy; 2022 Hapag: Filipino Cuisine and Recipe <br/> Gerson O. Pantoja Masiga, Gasan, Marinduque - Philippines <br/> Tel No. 0923 1343 342. All rights reserved.</b></p>
+
+			<div class="icons">
+				<ul><center>
+					<a href="#"><img src="images/icons/facebook.png"></a>
+					<a href="#"><img src="images/icons/instagram.png"></a>
+					<a href="#"><img src="images/icons/pinterest.png"></a>
+					<a href="#"><img src="images/icons/youtube.png"></a>
+				</center></ul>
+			</div>
+			
+
+		</div>
+	</div>
+
+</body>
+</html>
